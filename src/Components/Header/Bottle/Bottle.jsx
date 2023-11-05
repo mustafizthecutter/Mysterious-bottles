8fe52295f5ef8a240b85b3b555a187abb82d2189
Mustafizur Rahman <mustafizur232@gmail.com>
2023-11-05 09:42:13 +0600

    shopping cart added by clicking purchase button

--- src/Components/Header/Bottle/Bottle.jsx
+++ src/Components/Header/Bottle/Bottle.jsx
@@ -1,11 +1,13 @@
 import './Bottle.css'
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle, handlePurchaseBtn }) => {
     const { name, price, img } = bottle
+ 
     return (
         <div className="bottle">
             <img src={img} alt="" />
             <h3>Bottle Name: {name}</h3>
             <p>Price: {price}</p>
+            <button onClick={() => handlePurchaseBtn(bottle)}>Purchase</button>
         </div>
     );
 };
--- src/Components/Header/Bottles/Bottles.jsx
+++ src/Components/Header/Bottles/Bottles.jsx
@@ -5,17 +5,22 @@ import './Bottles.css'
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([])
     useEffect(() => {
         fetch(`bottles.json`)
             .then(res => res.json())
             .then(data => setBottles(data))
     }, [])
+    const handlePurchaseBtn = bottle => {
+        setCart([...cart, bottle])
+    }
     return (
         <div>
             <h3>Bottles List : {bottles.length}</h3>
+            <h3>Cart: {cart.length}</h3>
             <div className="box-container">
                 {
-                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                    bottles.map(bottle => <Bottle handlePurchaseBtn={handlePurchaseBtn} key={bottle.id} bottle={bottle}></Bottle>)
                 }
             </div>
         </div>
